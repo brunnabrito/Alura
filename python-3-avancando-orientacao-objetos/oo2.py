059682f7d2934b6a2d4c1ceca1b1509292f3770c
Brunna Brito <brunna_brito@yahoo.com.br>
2021-12-07 16:57:22 -0300

    seguindo com as aulas

--- python-3-avancando-orientacao-objetos/oo2.py
+++ python-3-avancando-orientacao-objetos/oo2.py
@@ -3,43 +3,89 @@ class Programa:
         self._nome = nome.title()
         self.ano = ano
         self._likes = 0
-    
+
     @property
     def likes(self):
         return self._likes
 
-    def dar_like(self):
+    def dar_likes(self):
         self._likes += 1
-    
+
     @property
     def nome(self):
-        self._nome
+        return self._nome
 
     @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+    def nome(self, nome):
+        self._nome = nome
 
+    def __str__(self):
+        return f'Nome: {self.nome} Likes: {self.likes}'
 
 class Filme(Programa):
-   def __init__(self, nome, ano, duracao):
+    def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
-        
+    
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
 
 
-vingadores = Filme('vingadores - guerra infinita', 2018, 240)
-vingadores.dar_like()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Temporadas: {vingadores.duracao} - Likes: {vingadores.likes}')
+class Playlist():
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+    
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    def __len__(self):
+        return len(self._programas)
 
 
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
-atlanta.dar_like()
-atlanta.dar_like()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+tmep = Filme('todo mundo em panico', 1999, 100)
+demolidor = Serie('demolidor', 2016, 2)
+
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+atlanta.dar_likes()
+atlanta.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+
+listinha = [atlanta, vingadores, demolidor, tmep]
+minha_playlist = Playlist('fim de semana', listinha)
+
+for programa in minha_playlist:
+    print(programa)
+
+print(f'Tamanho: {len(minha_playlist)}')
+
+len(minha_playlist)
+
+print(minha_playlist[0])
+
+"""Inicialização	__init__
+Representação	__str__, __repr__
+Container, sequência	__contains__, __iter__, __len__, __getitem__
+Numéricos	__add__, __sub__, __mul__, __mod__
+"""
+
 
